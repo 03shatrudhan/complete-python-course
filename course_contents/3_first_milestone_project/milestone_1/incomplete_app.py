68450a47968ac19f344e9d8d31ea89648f6a6a31
03shatrudhan <118226461+03shatrudhan@users.noreply.github.com>
2022-11-17 06:06:48 +0530

    Update incomplete_app.py

--- course_contents/3_first_milestone_project/milestone_1/incomplete_app.py
+++ course_contents/3_first_milestone_project/milestone_1/incomplete_app.py
@@ -1,39 +1,48 @@
-# Incomplete app!
 
 MENU_PROMPT = "\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
 movies = []
 
 
 # You may want to create a function for this code
-title = input("Enter the movie title: ")
-director = input("Enter the movie director: ")
-year = input("Enter the movie release year: ")
+def add_movies():
+    
+    title = input("Enter the movie title: ")
+    director = input("Enter the movie director: ")
+    year = input("Enter the movie release year: ")
 
-movies.append({
-    'title': title,
-    'director': director,
-    'year': year
-})
+    movies.append({
+        'title': title,
+        'director': director,
+        'year': year
+    })
+    
+
+def list_movies(movies):
+    print(movies)
+    
+
+def  find_movies():
+        show_movie(movies)
+        
+        
+def show_movie(movie):
+     for movi in movie:
+            print("Movie title",movi['title'])
+            print("Movie director",movi['director'])
+            print("Realeased Movie year",movi["year"])
 
 
-# Create other functions for:
-#   - listing movies
-#   - finding movies
-
-
-# And another function here for the user menu
-selection = input(MENU_PROMPT)
-while selection != 'q':
-    if selection == "a":
-        pass
-    elif selection == "l":
-        pass
-    elif selection == "f":
-        pass
-    else:
-        print('Unknown command. Please try again.')
-
+def select_menu():
     selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection == "a":
+            add_movies()
+        elif selection == "l":
+           list_movies(movies)
+        elif selection == "f":
+           find_movies() 
+        else:
+            print('Unknown command. Please try again.')
 
-
-# Remember to run the user menu function at the end!
+        selection = input(MENU_PROMPT)
+select_menu()
